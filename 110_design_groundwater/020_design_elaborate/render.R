aa8be5148b0f8866292be27b0c90744f5cd16b70
florisvdh <floris.vanderhaeghe@inbo.be>
2020-08-11 14:48:25 +0200

    Design gw, EDA: accomodate reading analdata from disk

--- 110_design_groundwater/020_design_elaborate/render.R
+++ 110_design_groundwater/020_design_elaborate/render.R
@@ -1,2 +1,5 @@
 bookdown::render_book("index.Rmd",
                       "bookdown::html_document2")
+
+# Run in shell to invoke non-interactive R session:
+# Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(compile_analysisdata = FALSE))'
