20bb5c231a6bca674392d69f2bc3ae7689d98f5f
florisvdh <floris.vanderhaeghe@inbo.be>
2020-08-12 09:18:40 +0200

    Design gw: extend render.R

--- 110_design_groundwater/020_design_elaborate/render.R
+++ 110_design_groundwater/020_design_elaborate/render.R
@@ -3,3 +3,4 @@ bookdown::render_book("index.Rmd",
 
 # Run in shell to invoke non-interactive R session:
 # Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(compile_analysisdata = FALSE))'
+# Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(connect_watina = TRUE))'
