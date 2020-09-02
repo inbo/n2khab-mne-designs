9ed0a1782178d5cdc31de0d88c246366afd96b2c
florisvdh <floris.vanderhaeghe@inbo.be>
2020-09-02 16:30:23 +0200

    render.R: add statement

--- 110_design_groundwater/020_design_elaborate/render.R
+++ 110_design_groundwater/020_design_elaborate/render.R
@@ -1,5 +1,8 @@
 bookdown::render_book("index.Rmd",
                       "bookdown::html_document2")
+bookdown::render_book("index.Rmd",
+                      "bookdown::html_document2",
+                      params = list(compile_analysisdata = FALSE))
 
 # Run in shell to invoke non-interactive R session:
 # Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(compile_analysisdata = FALSE))'
