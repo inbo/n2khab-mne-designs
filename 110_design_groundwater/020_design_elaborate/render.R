3d20afe177a4d9fa94f69aa15e210fc25da83257
florisvdh <floris.vanderhaeghe@inbo.be>
2020-10-13 17:47:56 +0200

    Design gw, envdata: rename parameter to write_analysisdata

--- 110_design_groundwater/020_design_elaborate/render.R
+++ 110_design_groundwater/020_design_elaborate/render.R
@@ -2,8 +2,8 @@ bookdown::render_book("index.Rmd",
                       "bookdown::html_document2")
 bookdown::render_book("index.Rmd",
                       "bookdown::html_document2",
-                      params = list(compile_analysisdata = FALSE))
+                      params = list(write_analysisdata = FALSE))
 
 # Run in shell to invoke non-interactive R session:
-# Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(compile_analysisdata = FALSE))'
+# Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(write_analysisdata = FALSE))'
 # Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2", params = list(connect_watina = TRUE))'
