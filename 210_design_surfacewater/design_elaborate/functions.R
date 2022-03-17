fc27cc9228c6140240a7821dffb1a12fd7652161
DE DOBBELAER <tom.dedobbelaer@inbo.be>
2022-03-17 12:31:24 +0100

    add elaborate Rmd

---
+++ 210_design_surfacewater/design_elaborate/functions.R
@@ -0,0 +1,111 @@
+################################################################################
+
+# Miscellaneous functions
+
+execshell <- function(commandstring, intern = FALSE) {
+    if (.Platform$OS.type == "windows") {
+        res <- shell(commandstring, intern = TRUE)
+    } else {
+        res <- system(commandstring, intern = TRUE)
+    }
+    if (!intern) cat(res, sep = "\n") else return(res)
+}
+
+#####################################################################
+#corvif() FUNCTION.
+#From:
+    #Mixed effects models and extensions in ecology with R. (2009).
+    #Zuur, AF, Ieno, EN, Walker, N, Saveliev, AA, and Smith, GM. Springer.
+#Modified by @florisvdh
+
+corvif <- function(dataz) {
+    dataz <- as.data.frame(dataz)
+
+    #vif part
+    form    <- formula(paste("fooy ~ ",paste(strsplit(names(dataz)," "),collapse=" + ")))
+    dataz   <- data.frame(fooy=1 + rnorm(nrow(dataz)) ,dataz)
+    lm_mod  <- lm(form,dataz)
+
+    vif <- myvif(lm_mod)
+    covariates <- rownames(vif)
+    vif %>%
+        dplyr::as_tibble() %>%
+        dplyr::select(last_col()) %>%
+        dplyr::mutate(covariate = covariates) %>%
+        dplyr::rename(vif = 1) %>%
+        dplyr::select(2, 1)
+}
+
+#Support function for corvif. Will not be called by the user
+myvif <- function(mod) {
+    v <- vcov(mod)
+    assign <- attributes(model.matrix(mod))$assign
+    if (names(coefficients(mod)[1]) == "(Intercept)") {
+        v <- v[-1, -1]
+        assign <- assign[-1]
+    } else warning("No intercept: vifs may not be sensible.")
+    terms <- labels(terms(mod))
+    n.terms <- length(terms)
+    if (n.terms < 2) stop("The model contains fewer than 2 terms")
+    if (length(assign) > dim(v)[1] ) {
+        diag(tmp_cor)<-0
+        if (any(tmp_cor==1.0)){
+            return("Sample size is too small, 100% collinearity is present")
+        } else {
+            return("Sample size is too small")
+        }
+    }
+    R <- cov2cor(v)
+    detR <- det(R)
+    result <- matrix(0, n.terms, 3)
+    rownames(result) <- terms
+    colnames(result) <- c("GVIF", "Df", "GVIF^(1/2Df)")
+    for (term in 1:n.terms) {
+        subs <- which(assign == term)
+        result[term, 1] <- det(as.matrix(R[subs, subs])) * det(as.matrix(R[-subs, -subs])) / detR
+        result[term, 2] <- length(subs)
+    }
+    if (all(result[, 2] == 1)) {
+        result <- data.frame(GVIF=result[, 1])
+    } else {
+        result[, 3] <- result[, 1]^(1/(2 * result[, 2]))
+    }
+    return(result)
+}
+#END VIF FUNCTIONS
+
+
+################################################################################
+
+# Functions used to retrieve file metadata
+
+get_latest_filecommit <- function(filepath, reporoot, repostatus) {
+    label <-
+        execshell(paste0("git log -n 1 --pretty=format:'`%H` (%ai)' -- '",
+                         filepath, "'"),
+           intern = TRUE)
+    gitpath <- fs::path_rel(filepath, reporoot)
+    if (gitpath %in% repostatus$unstaged) {
+        paste("UNCOMMITTED! Modified relative to latest file commit:",
+              label) %>%
+            return
+    } else return(label)
+}
+
+get_vc_datahash <- function(datafile) {
+    file_extension <- substr(datafile, nchar(datafile) - 3, 1000000L)
+
+    if (substr(file_extension, 1, 1) != ".") {
+        datafile <- paste0(datafile, ".yml")
+    } else {
+        if (file_extension != ".yml") {
+        stop("You must provide a vc-data object name or the yml file name.")
+        }
+    }
+
+    yaml::read_yaml(datafile) %>%
+    .$..generic %>%
+    .$data_hash
+}
+
+enclose <- function(x, y) paste0(y, x, y)
