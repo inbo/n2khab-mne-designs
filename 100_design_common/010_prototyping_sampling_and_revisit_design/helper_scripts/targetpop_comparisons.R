277d72e3bb122acb9446562f4a07b74943d7464f
florisvdh <floris.vanderhaeghe@inbo.be>
2024-01-19 15:43:13 +0100

    POC: add helper script to compare different targetpops

---
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/targetpop_comparisons.R
@@ -0,0 +1,88 @@
+schemes <-
+  read_schemes(lang = lang) %>%
+  filter(attribute_1 %in% c("GW", "SURF", "SOIL")) %>%
+  select(scheme, programme, attribute_1, attribute_2, attribute_3, tag_1, spatial_restriction)
+
+core_schemes_1st_wave <- c(
+  "GW_03.3",
+  "SOIL_03.2",
+  "SURF_03.4_lentic",
+  "SURF_03.4_lotic"
+)
+
+schemes_1st_wave <- c(
+  core_schemes_1st_wave,
+  "GW_04.2",
+  "GW_05.1_aq",
+  "GW_05.1_quarries",
+  "GW_05.1_terr",
+  "GW_05.2",
+  "GW_07.1",
+  "SOIL_08.2",
+  "SURF_07.2"
+)
+
+targetpop <-
+  read_scheme_types(lang = lang) %>%
+  select(scheme, type) %>%
+  semi_join(schemes, by = "scheme")
+
+# MHQ has no unique types relative to the MNE target population for GW+SURF+SOIL
+read_scheme_types() %>%
+  select(scheme, type) %>%
+  semi_join(
+    read_schemes() %>%
+      filter(programme == "MHQ"),
+    by = "scheme"
+  ) %>%
+  anti_join(targetpop, by = "type")
+
+# Which types belong to targetpop of schemes_1st_wave and NOT to targetpop of core_schemes_1st_wave?
+# This also needs the stratum_props object from 'type_exploration_to_select_in_poc.R'
+non_core_types <-
+  read_scheme_types(lang = lang) %>%
+  select(scheme, type, typegroup, typegroup_shortname) %>%
+  filter(scheme %in% schemes_1st_wave) %>%
+  left_join(
+    read_scheme_types() %>%
+      count(scheme, typegroup, name = "group_size"),
+    by = c("scheme", "typegroup")
+  ) %>%
+  # distinct(type) %>%
+  anti_join(
+    read_scheme_types(lang = lang) %>%
+      select(scheme, type) %>%
+      filter(scheme %in% core_schemes_1st_wave) %>%
+      distinct(type),
+    by = "type"
+  ) %>%
+  left_join(
+    stratum_props %>%
+      select(type, stratum, population_size, sample_support) %>%
+      inner_join(
+        read_types(lang = lang) %>%
+          select(type, type_shortname),
+        by = "type"
+      ),
+    by = "type",
+    relationship = "many-to-many"
+  ) %>%
+  relocate(type_shortname, .after = type) %>%
+  arrange(scheme, typegroup, type, stratum)
+
+# Maintaining typegroep precision for a typegroup of k types, when p types are removed,
+# necessitates sample size increase of each remaining type by a factor k / (k - p)
+
+library(ggplot2)
+crossing(p = 0:5, k = 10:30) %>%
+  mutate(relative_sample_size_increase = k / (k - p)) %>%
+  ggplot(aes(x = k, y = p, fill = relative_sample_size_increase)) +
+  geom_tile() +
+  geom_text(aes(label = round(relative_sample_size_increase, 2)), color = "white") +
+  scale_fill_viridis_c(option = "plasma") +
+  labs(
+    x = "Number of types in a typegroup",
+    y = "Number of types removed from a typegroup",
+    fill = "Needed relative\nsample size increase"
+  )
+
