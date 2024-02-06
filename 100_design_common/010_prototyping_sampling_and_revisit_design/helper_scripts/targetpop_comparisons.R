951f10fe797b0b1139d5919eb11fa1918146e6a6
florisvdh <floris.vanderhaeghe@inbo.be>
2024-02-06 11:22:47 +0100

    POC, 010_settings: update schemes & targetpop restrictions

--- 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/targetpop_comparisons.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/targetpop_comparisons.R
@@ -3,22 +3,21 @@ schemes <-
   filter(attribute_1 %in% c("GW", "SURF", "SOIL")) %>%
   select(scheme, programme, attribute_1, attribute_2, attribute_3, tag_1, spatial_restriction)
 
-core_schemes_1st_wave <- c(
+core_schemes_1st_phase <- c(
   "GW_03.3",
   "SOIL_03.2",
   "SURF_03.4_lentic",
   "SURF_03.4_lotic"
 )
 
-schemes_1st_wave <- c(
-  core_schemes_1st_wave,
+schemes_1st_phase <- c(
+  core_schemes_1st_phase,
   "GW_04.2",
   "GW_05.1_aq",
-  "GW_05.1_quarries",
-  "GW_05.1_terr",
+  "GW_05.1_terr", # don't take the whole target population here!! Only groups 2, 3, 5
   "GW_05.2",
   "GW_07.1",
-  "SOIL_08.2",
+  "SURF_064_lentic", # this one's existence is still to be confirmed
   "SURF_07.2"
 )
 
@@ -26,6 +25,18 @@ targetpop <-
   read_scheme_types(lang = lang) %>%
   select(scheme, type) %>%
   semi_join(schemes, by = "scheme")
+
+targetpop_exclude_from_1st_phase <-
+  read_scheme_types(lang = lang) %>%
+  filter(
+    scheme == "GW_05.1_terr",
+    str_detect(typegroup, "GW_05.1_terr_group(1|4)")
+  ) %>%
+  select(scheme, type)
+
+targetpop <-
+  targetpop %>%
+  anti_join(targetpop_exclude_from_1st_phase, by = c("scheme", "type"))
 
 # MHQ has no unique types relative to the MNE target population for GW+SURF+SOIL
 read_scheme_types() %>%
@@ -37,12 +48,12 @@ read_scheme_types() %>%
   ) %>%
   anti_join(targetpop, by = "type")
 
-# Which types belong to targetpop of schemes_1st_wave and NOT to targetpop of core_schemes_1st_wave?
+# Which types belong to targetpop of schemes_1st_phase and NOT to targetpop of core_schemes_1st_phase?
 # This also needs the stratum_props object from 'type_exploration_to_select_in_poc.R'
 non_core_types <-
   read_scheme_types(lang = lang) %>%
   select(scheme, type, typegroup, typegroup_shortname) %>%
-  filter(scheme %in% schemes_1st_wave) %>%
+  filter(scheme %in% schemes_1st_phase) %>%
   left_join(
     read_scheme_types() %>%
       count(scheme, typegroup, name = "group_size"),
@@ -52,7 +63,7 @@ non_core_types <-
   anti_join(
     read_scheme_types(lang = lang) %>%
       select(scheme, type) %>%
-      filter(scheme %in% core_schemes_1st_wave) %>%
+      filter(scheme %in% core_schemes_1st_phase) %>%
       distinct(type),
     by = "type"
   ) %>%
@@ -86,7 +97,7 @@ crossing(p = 0:5, k = 10:30) %>%
     fill = "Needed relative\nsample size increase"
   )
 
-# The MNE + MHQ '1st wave' targetpop comprises all types targeted by scheme
+# The MNE + MHQ '1st phase' targetpop comprises all types targeted by scheme
 # ATM_03.1
 
 schemes <- read_schemes(lang = lang) %>%
