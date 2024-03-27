65e93668e99054dc6b102e3cd15390774ca208d1
florisvdh <floris.vanderhaeghe@inbo.be>
2024-03-27 15:01:23 +0100

    POC, 040_scheme_obj: rename object to module_domain_scheme_designattr

--- 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/writing_to_googlesheet.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/writing_to_googlesheet.R
@@ -137,7 +137,7 @@ non_core_types_per_module_and_compartment %>%
 # Below code requires availability of:
 # - module_domain_scheme_design
 
-module_domain_scheme_design %>%
+module_domain_scheme_designattr %>%
   select(module, domain, scheme, cycle_duration_y, type_count, type_count_for_sampling, sp_sample_size_all_panels) %>%
   mutate(yearly_sample_size = round(sp_sample_size_all_panels / cycle_duration_y)) %>%
   write_sheet(
