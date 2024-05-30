12118eca5253d9fbfc324b7e868ba93a2469902c
florisvdh <floris.vanderhaeghe@inbo.be>
2024-05-30 11:22:11 +0200

    DCP MBAA, 050_sps: add maps of spatial samples

--- 100_design_common/020_data_collection_plan_mbaa/R/functions.R
+++ 100_design_common/020_data_collection_plan_mbaa/R/functions.R
@@ -12,3 +12,32 @@ filter_grts_mh_by_address <- function(
     ifel(!is.na(r), TRUE, r)
   } else r
 }
+
+add_point_coords_grts <- function(
+    df,
+    grts_var = "grts_address",
+    spatrast = grts_mh_n2khab,
+    spatrast_index = grts_mh_n2khab_index,
+    spatial = TRUE) {
+
+  addresses <- df %>%
+    distinct(.data[[grts_var]]) %>%
+    pull(.data[[grts_var]]) %>%
+    sort()
+
+  grts_cells <- spatrast_index %>%
+    filter(grts_address %in% addresses) %>%
+    arrange(grts_address) %>%
+    pull(id)
+
+  coords <- terra::xyFromCell(spatrast, grts_cells)
+
+  df %>%
+    left_join(
+      tibble(grts_address = addresses, x = coords[,"x"], y = coords[,"y"]),
+      join_by(grts_address)
+    ) %>%
+    {if (isFALSE(spatial)) . else {
+      st_as_sf(., coords = c("x", "y"), crs = crs(spatrast))
+    }}
+}
