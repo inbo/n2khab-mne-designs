4330199d46e868dd86dfd7f9f6bbdd062097dd33
florisvdh <floris.vanderhaeghe@inbo.be>
2024-05-30 18:30:38 +0200

    POC: add extra object to be saved in RData file

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
@@ -11,6 +11,7 @@ rdata_set <- c(
   "n2khab_targetpops_expanded",
   "n2khab_targetpops",
   "grts_mh_n2khab_index",
+  "domain_grts_n2khab",
   "stratum_grts_n2khab",
   "stratum_grts_n2khab_collapsed",
   "domain_stratum_nunits",
