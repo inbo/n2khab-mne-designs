1460844d633988fc1d239253193783d8bd6f51dd
florisvdh <floris.vanderhaeghe@inbo.be>
2024-06-11 14:12:16 +0200

    DCP MBAA, 050_sps: refactor SAC density graph, using facets

--- 100_design_common/020_data_collection_plan_mbaa/R/functions.R
+++ 100_design_common/020_data_collection_plan_mbaa/R/functions.R
@@ -36,6 +36,17 @@ our_update_theme_revisitdiag_horfacets <- function() theme(
   strip.text.y = element_text(angle = 0),
   axis.ticks = element_blank()
 )
+our_update_theme_bars_in_horfacets <- function() theme(
+  panel.background = element_blank(),
+  panel.grid.major.y = element_blank(),
+  panel.grid.major.x = element_line(colour = col_gridline_bw),
+  panel.grid.minor.x = element_line(colour = col_gridline_bw),
+  axis.ticks.y = element_blank(),
+  axis.text.y = element_blank(),
+  panel.border = element_rect(colour = col_gridline_bw, fill = NA),
+  strip.text.y = element_text(angle = 0),
+  legend.position = "top"
+)
 
 filter_grts_mh_by_address <- function(
     addresses,
