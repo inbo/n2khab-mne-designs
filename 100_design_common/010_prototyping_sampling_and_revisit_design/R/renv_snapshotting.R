42df0815eef4ca3500b318bb36b295592e21d71e
florisvdh <floris.vanderhaeghe@inbo.be>
2024-10-01 10:03:27 +0200

    POC, renv_snapshotting.R: update comments & style

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/renv_snapshotting.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/renv_snapshotting.R
@@ -3,13 +3,14 @@
 # below. (First-time setup: run renv::activate() and then remove .Rprofile)
 
 source("renv/activate.R") # this is 'activating renv on demand'
+# if asked 'Would you like to restore the project library?', answer N
 renv::upgrade() # makes sure latest renv version is in use
 if (file.exists(".Rprofile")) unlink(".Rprofile")
-renv::hydrate(update = "all") # populates or updates renv project library with
-                              # the package versions used when renv is
-                              # not active
-renv::snapshot() # records packages with their versions in renv.lock
+# populate or update renv project library with the package versions used when
+# renv is not active:
+renv::hydrate(update = "all")
 # renv::install("yaml") # links a missing package in the renv project library
+renv::snapshot() # records packages with their versions in renv.lock
 # renv::record("yaml") # records a renv project library package in renv.lock
 
 # then commit all changes
