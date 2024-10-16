533d8dd76246824d1bd2cc7705742613c743a4b2
florisvdh <floris.vanderhaeghe@inbo.be>
2024-10-16 15:23:46 +0200

    POC, 010_settings: refactor, expand & restructure panel designs

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
@@ -1,3 +1,71 @@
+apply_activity_sequence_filters <- function(df) {
+  df %>%
+    filter(
+      # for GW_03.3 x GWLEVREADDIVER as core scheme, use the piezwell sequence:
+      !(
+        scheme == "GW_03.3" & is_core_scheme &
+          main_field_method == "GWLEVREADDIVER" &
+          ((in_aquatic_subset & activity_sequence != "gwsurf_lev_piezwell") |
+             (!in_aquatic_subset & activity_sequence != "gw_lev_piezwell"))
+      ),
+      # for GW_03.3 x GWSHALLSAMP as core scheme, use the readman sequence:
+      !(
+        scheme == "GW_03.3" & is_core_scheme &
+          main_field_method == "GWSHALLSAMP" &
+          ((in_aquatic_subset & activity_sequence != "gw_samp_gwsurf_readman") |
+             (!in_aquatic_subset & activity_sequence != "gw_samp_gw_readman"))
+      ),
+      # for other GWSHALLSAMP, use the gw_samp sequence:
+      !(
+        (scheme != "GW_03.3" | !is_core_scheme) &
+          main_field_method == "GWSHALLSAMP" &
+          activity_sequence != "gw_samp"
+      ),
+      # for GW_05.1_aq x (GWLEVREADDIVER or SURFLEVREADGAUGE) as non-core
+      # scheme, only include sequence gwsurf_lev_piezwell
+      !(
+        scheme == "GW_05.1_aq" & !is_core_scheme &
+          main_field_method %in% c("GWLEVREADDIVER", "SURFLEVREADGAUGE") &
+          activity_sequence != "gwsurf_lev_piezwell"
+      ),
+      # for GW_05.1_terr, GW_05.2 x GWLEVREADDIVER as non-core scheme, only
+      # include sequence gw_lev_piezwell
+      !(
+        scheme %in% c("GW_05.1_terr", "GW_05.2") & !is_core_scheme &
+          main_field_method == "GWLEVREADDIVER" &
+          activity_sequence != "gw_lev_piezwell"
+      ),
+      # for GW_05.1_aq x GWLEVREADDIVER as core scheme, only include sequence
+      # gwsurf_lev_well
+      !(
+        scheme == "GW_05.1_aq" & is_core_scheme &
+          main_field_method == "GWLEVREADDIVER" &
+          activity_sequence != "gwsurf_lev_well"
+      ),
+      # for GW_05.1_terr, GW_05.2 x GWLEVREADDIVER as core scheme, only include
+      # sequence gw_lev_well
+      !(
+        scheme %in% c("GW_05.1_terr", "GW_05.2") & is_core_scheme &
+          main_field_method == "GWLEVREADDIVER" &
+          activity_sequence != "gw_lev_well"
+      ),
+      # for SURF_03.4_lentic x SURFLEVREADGAUGE, use the surf_lent_samp
+      # sequence:
+      !(
+        scheme == "SURF_03.4_lentic"  &
+          main_field_method == "SURFLEVREADGAUGE" &
+          activity_sequence != "surf_lent_samp"
+      ),
+      # for SURF_03.4_lotic x SURFLEVREADGAUGE, use the surf_lent_samp sequence:
+      !(
+        scheme == "SURF_03.4_lotic"  &
+          main_field_method == "SURFLEVREADGAUGE" &
+          activity_sequence != "surf_lot_samp"
+      )
+    )
+}
+
+
 collapse_strata <- function(df) {
   df %>%
     mutate(
