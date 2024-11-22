a67523c9b13323e26cb6932800e763c594c5be62
florisvdh <floris.vanderhaeghe@inbo.be>
2024-11-22 17:15:00 +0100

    POC, 050_sts: update sf output object names & content

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
@@ -37,7 +37,7 @@ rdata_set <- c(
   "module_domain_scheme_stratum_target_sample_size",
   "module_domain_scheme_stratum_sample_size",
   "scheme_domain_stratum_spsamples",
-  "scheme_domain_stratum_spsamples_sf",
+  "scheme_stratum_spsamples_sf",
   "samplinglocations_sf",
   "scheme_fag_fa",
   "scheme_domain_fag_panel_count",
