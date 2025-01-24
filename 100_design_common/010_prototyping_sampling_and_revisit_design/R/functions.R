fce585ac3dc27f1035bf0e854452783c63b1141b
florisvdh <floris.vanderhaeghe@inbo.be>
2025-01-24 10:29:04 +0100

    POC, simplify_period(): convert NA to NA_character_ *
    This is needed to be used inside map_chr(), since the latter doesn't like to coerce itself.
    
    The warning emerged in the creation of scheme_moco_ps_spsubset_fas_panelcount_full:
    
    Warning message:
    There was 1 warning in `mutate()`.
    ℹ In argument: `panel_enddate_addition_chr = map_chr(panel_enddate_addition, simplify_period)`.
    Caused by warning:
    ! Automatic coercion from double to character was deprecated in purrr 1.0.0.
    ℹ Please use an explicit call to `as.character()` within `map_chr()` instead.

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
@@ -451,7 +451,7 @@ generate_spare_units <- function(ssf_sample, coef_spare) {
 #'
 #' @param x A scalar of class 'Period'.
 simplify_period <- function(x) {
-  if (is.na(x)) return(x)
+  if (is.na(x)) return(NA_character_)
   as.character(x) %>%
     str_split_1(" ") %>%
     {.[!str_detect(., "^0")]}
