7591858fdb3d61046ab36d4b315ee259333a91f2
florisvdh <floris.vanderhaeghe@inbo.be>
2025-01-29 15:45:27 +0100

    POC, save_data.R: use module shortcodes in file name

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/save_rdata.R
@@ -53,11 +53,7 @@ rdata_set <- c(
   "fag_stratum_grts_calendar",
   "fag_grts_calendar"
 )
-module_suffix <- if (length(params$active_modules) > 1) {
-  ""
-} else {
-  str_c("_", params$active_modules)
-}
+module_suffix <- str_c("_", str_flatten(modules$code_short))
 save(
   list = rdata_set,
   file = file.path(
