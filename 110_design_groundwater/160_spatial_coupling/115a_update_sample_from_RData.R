83024cd00a06e7f7c8c7d5f2edd2e6c26d6c97a0
Falk Mielke <falk.mielke@inbo.be>
2025-05-19 09:48:42 +0200

    spatialcoupling: auto-load mne sample RData

---
+++ 110_design_groundwater/160_spatial_coupling/115a_update_sample_from_RData.R
@@ -0,0 +1,65 @@
+library("googledrive")
+library("dplyr")
+library("sf")
+library("n2khab")
+
+#### google authentification
+# Setup for googledrive authentication. Set the appropriate env vars in
+# .Renviron and make sure you ran drive_auth() interactively with these settings
+# for the first run (or to renew an expired Oauth token).
+# See ?gargle::gargle_options for more information.
+if (Sys.getenv("GARGLE_OAUTH_EMAIL") != "") {
+  options(gargle_oauth_email = Sys.getenv("GARGLE_OAUTH_EMAIL"))
+}
+if (Sys.getenv("GARGLE_OAUTH_CACHE") != "") {
+  options(gargle_oauth_cache = Sys.getenv("GARGLE_OAUTH_CACHE"))
+}
+
+
+# https://drive.google.com/file/d/1a42qESF5L8tfnEseHXbTn9hYR1phqS-S/view?usp=drive_link
+path <- file.path(tempdir(), "objects_panflpan5.RData")
+googledrive::drive_download(as_id("1a42qESF5L8tfnEseHXbTn9hYR1phqS-S"), path = path)
+# https://drive.google.com/file/d/1a42qESF5L8tfnEseHXbTn9hYR1phqS-S/view?usp=drive_link
+
+# load the data into a new environment
+env_extradata <- new.env()
+load(path, envir = env_extradata)
+
+# ls(envir = env_extradata)
+
+get_variable <- function(varname) get(varname, envir = env_extradata)
+# units_locations <- get_variable("scheme_moco_ps_stratum_sppost_spsamples_sf")
+# get_variable("mhq_samples")
+# ? get_variable("sp_samplingframes")
+#   samfra <- get_variable("sp_samplingframes")
+#   samfra[[1, "sampling_frame"]]
+# get_variable("samplinglocations_sf")
+# get_variable("n2khab_strata")
+
+# get_variable("mhq_scheme_category") %>%
+#   distinct(scheme)
+sample <- get_variable("mhq_samples") %>%
+  inner_join(
+    get_variable("n2khab_strata"),
+    join_by(stratum),
+    unmatched = c("error", "drop"),
+    relationship = "many-to-many"
+  ) %>%
+  inner_join(
+    n2khab::read_types(),
+    join_by(type),
+    unmatched = c("error", "drop"),
+    relationship = "many-to-many"
+  ) %>%
+  left_join(
+    get_variable("samplinglocations_sf"),
+    join_by(grts_address == grts_address_final),
+    relationship = "many-to-one"
+  ) %>%
+  select(stratum, grts_address, hydr_class, hydr_class_shortname, geometry)
+# df: {stratum, grts_address, hydr_class, hydr_class_shortname, geometry}
+
+sample <- sf::st_as_sf(sample)
+
+store_filepath <- file.path("./cache", "spsamples_gw_sf_panflpan5.rds")
+saveRDS(sample, store_filepath)
