0b5cf84c9a0f0d04975108e844f971b73e06ac8a
Falk Mielke <falk.mielke@inbo.be>
2025-05-19 13:25:54 +0200

    spatialcoupling: correcting sample data

--- 110_design_groundwater/160_spatial_coupling/115a_update_sample_from_RData.R
+++ 110_design_groundwater/160_spatial_coupling/115a_update_sample_from_RData.R
@@ -36,9 +36,41 @@ get_variable <- function(varname) get(varname, envir = env_extradata)
 # get_variable("samplinglocations_sf")
 # get_variable("n2khab_strata")
 
+## <https://github.com/inbo/n2khab-mne-design/blob/poc/100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_dcp_and_fieldwork.R>
+# scheme_moco_ps_stratum_sppost_spsamples_sf %>%
+#   filter(str_detect(scheme, "^(GW)")) %>%
+#   distinct(stratum, grts_address, geometry) %>%
+#   # adding type metadata
+#   inner_join(
+#     n2khab_strata,
+#     join_by(stratum),
+#     relationship = "many-to-one",
+#     unmatched = c("error", "drop")
+#   ) %>%
+#   inner_join(
+#     read_types() %>%
+#       select(
+#         type,
+#         hydr_class,
+#         hydr_class_shortname
+#       ),
+#     join_by(type),
+#     relationship = "many-to-one",
+#     unmatched = c("error", "drop")
+#   ) %>%
+#   select(-type) %>%
+#   relocate(geometry, .after = last_col())
+
 # get_variable("mhq_scheme_category") %>%
+#  left_join(
+#    get_variable("samplinglocations_sf"),
+#    join_by(grts_address == grts_address_final),
+#    relationship = "many-to-one"
+#  ) %>%
 #   distinct(scheme)
-sample <- get_variable("mhq_samples") %>%
+sample <- get_variable("scheme_moco_ps_stratum_sppost_spsamples_sf") %>%
+  # cbind(sf::st_drop_geometry(.), sf::st_coordinates(.)) %>%
+  filter(stringr::str_detect(scheme, "^(GW)")) %>%
   inner_join(
     get_variable("n2khab_strata"),
     join_by(stratum),
@@ -50,11 +82,6 @@ sample <- get_variable("mhq_samples") %>%
     join_by(type),
     unmatched = c("error", "drop"),
     relationship = "many-to-many"
-  ) %>%
-  left_join(
-    get_variable("samplinglocations_sf"),
-    join_by(grts_address == grts_address_final),
-    relationship = "many-to-one"
   ) %>%
   select(stratum, grts_address, hydr_class, hydr_class_shortname, geometry)
 # df: {stratum, grts_address, hydr_class, hydr_class_shortname, geometry}
