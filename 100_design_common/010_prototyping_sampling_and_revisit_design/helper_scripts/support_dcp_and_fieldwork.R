9357779375c2a2afcc32d4a554c823e27920c982
florisvdh <floris.vanderhaeghe@inbo.be>
2025-05-23 20:29:46 +0200

    POC, dcpfw-script, org: improve check of units_cell_rast

--- 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_dcp_and_fieldwork.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_dcp_and_fieldwork.R
@@ -339,7 +339,7 @@ stratum_schemetargetpanel_spsamples %>%
   filter(str_detect(sample_support_code, "cell")) %>%
   distinct(grts_address_final) %>%
   nrow() %>%
-  all.equal(global(units_cell_rast, "notNA") %>% as.integer())
+  all.equal(global(units_cell_rast, "notNA")[1, 1])
 
 # representing this limited number of cells as polygons: useful for plotting etc
 units_cell_polygon <-
