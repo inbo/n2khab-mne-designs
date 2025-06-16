5fbb582e16659f61d82a740c023045d48f2c9b08
florisvdh <floris.vanderhaeghe@inbo.be>
2025-06-16 13:09:28 +0200

    POC, sv896 script: update date & commit hash

--- 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_sv896.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_sv896.R
@@ -1,7 +1,7 @@
-# 2025-06-13 This code was written to support INBO's answer contribution of
+# 2025-06-16 This code was written to support INBO's answer contribution of
 # sv896.
 #
-# The results have been made with the RData file obtained at commit e5c05b34, by
+# The results have been made with the RData file obtained at commit 8d7b6942, by
 # running:
 #
 # Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::html_document2",
