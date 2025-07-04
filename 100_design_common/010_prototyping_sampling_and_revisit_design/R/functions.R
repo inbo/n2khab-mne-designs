a760df6c453422fc1e7d72860f5112a4620f3b12
florisvdh <floris.vanderhaeghe@inbo.be>
2025-07-04 16:36:37 +0200

    POC, collapse_strata(): add documentation

--- 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/R/functions.R
@@ -119,6 +119,21 @@ apply_activity_sequence_filters <- function(df) {
 }
 
 
+#' Collapse (unexpand) a data frame with a `stratum` column
+#'
+#' Collapses a data frame that has been the result of a n2khab::expand_types()
+#' operation.
+#'
+#' The collapsing (unexpanding) comprises two aspects:
+#'
+#' - replacing subtype codes by their main type code (where the target
+#' population is defined by the latter)
+#' - adding units, associated with a main type, to each corresponding subtype
+#' layer that triggered the expansion to the main type.
+#'
+#' In effect, 'collapsing' means leads to less stratum levels, but _more_ rows.
+#'
+#' @param df A data frame with a `stratum` column.
 collapse_strata <- function(df) {
   df %>%
     mutate(
