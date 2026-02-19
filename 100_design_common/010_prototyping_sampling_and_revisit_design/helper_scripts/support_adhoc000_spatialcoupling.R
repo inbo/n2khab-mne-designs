6522e0828e3f29cbb87b9c4bc1f1f68d87594597
florisvdh <floris.vanderhaeghe@inbo.be>
2026-02-19 11:56:21 +0100

    revisitplan, dcpfw-script, org: move out support for spatcoupling *
    This code stemmed from January 2025 and has been put in its own ad-hoc
    support script.
    
    The scope of the dcpfw-script is too wide and I want to narrow it to
    the preparatory code for the fieldwork organization code snippets in
    the n2khab-mne-monitoring repo.

---
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_adhoc000_spatialcoupling.R
@@ -0,0 +1,48 @@
+# Generating an object for inspection wrt spatial coupling of piezometers with
+# MNE sample
+
+# This code requires availability of the following objects:
+# - scheme_moco_ps_stratum_sppost_spsamples_sf
+# - n2khab_strata
+
+# First run setup chunk
+#
+# Then run:
+
+load(file.path(datapath, "binary/results/objects_panflpan5.RData"))
+
+spsamples_gw_sf_panflpan5 <-
+  scheme_moco_ps_stratum_sppost_spsamples_sf %>%
+  filter(str_detect(scheme, "^(GW)")) %>%
+  distinct(stratum, grts_address, geometry) %>%
+  # adding type metadata
+  inner_join(
+    n2khab_strata,
+    join_by(stratum),
+    relationship = "many-to-one",
+    unmatched = c("error", "drop")
+  ) %>%
+  inner_join(
+    read_types() %>%
+      select(
+        type,
+        hydr_class,
+        hydr_class_shortname
+      ),
+    join_by(type),
+    relationship = "many-to-one",
+    unmatched = c("error", "drop")
+  ) %>%
+  select(-type) %>%
+  relocate(geometry, .after = last_col())
+
+saveRDS(
+  spsamples_gw_sf_panflpan5,
+  file.path(datapath, "binary/intermediate/spsamples_gw_sf_panflpan5.rds")
+)
+
+write_sf(
+  spsamples_gw_sf_panflpan5,
+  file.path(datapath, "binary/intermediate/spsamples_gw_sf_panflpan5.gpkg")
+)
+
--- 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_dcp_and_fieldwork.R
+++ 100_design_common/010_prototyping_sampling_and_revisit_design/helper_scripts/support_dcp_and_fieldwork.R
@@ -4,64 +4,6 @@
 
 
 
-# Object wrt spatial coupling of piezometers with MNE sample --------
-
-
-
-# This code requires availability of the following objects:
-# - scheme_moco_ps_stratum_sppost_spsamples_sf
-# - n2khab_strata
-
-# First run setup chunk
-#
-# Then run:
-
-load(file.path(datapath, "binary/results/objects_panflpan5.RData"))
-
-spsamples_gw_sf_panflpan5 <-
-  scheme_moco_ps_stratum_sppost_spsamples_sf %>%
-  filter(str_detect(scheme, "^(GW)")) %>%
-  distinct(stratum, grts_address, geometry) %>%
-  # adding type metadata
-  inner_join(
-    n2khab_strata,
-    join_by(stratum),
-    relationship = "many-to-one",
-    unmatched = c("error", "drop")
-  ) %>%
-  inner_join(
-    read_types() %>%
-      select(
-        type,
-        hydr_class,
-        hydr_class_shortname
-      ),
-    join_by(type),
-    relationship = "many-to-one",
-    unmatched = c("error", "drop")
-  ) %>%
-  select(-type) %>%
-  relocate(geometry, .after = last_col())
-
-saveRDS(
-  spsamples_gw_sf_panflpan5,
-  file.path(datapath, "binary/intermediate/spsamples_gw_sf_panflpan5.rds")
-)
-
-write_sf(
-  spsamples_gw_sf_panflpan5,
-  file.path(datapath, "binary/intermediate/spsamples_gw_sf_panflpan5.gpkg")
-)
-
-
-
-
-
-
-
-
-
-
 # Tryout code to create data for piezometer positioning in aq types -------
 
 # This code requires availability of the following objects:
