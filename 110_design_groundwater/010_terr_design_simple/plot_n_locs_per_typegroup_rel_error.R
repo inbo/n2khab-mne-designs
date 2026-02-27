621f321c991af6b23eac22e795b70d230afc01c8
florisvdh <floris.vanderhaeghe@inbo.be>
2026-02-27 11:25:31 +0100

    gw_ssize: extend plot of rel_error & rel_detdiff vs n

--- 110_design_groundwater/010_terr_design_simple/plot_n_locs_per_typegroup_rel_error.R
+++ 110_design_groundwater/010_terr_design_simple/plot_n_locs_per_typegroup_rel_error.R
@@ -5,23 +5,19 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 library(forcats)
+library(stringr)
+library(purrr)
 
 # chunk copied from index.Rmd, containing the spatial variance (log-scale):
 modelvar <-
-    tibble(target_variable = c("HG3",
-                               "LG3",
-                               "N-NH4",
-                               "N-NO3",
-                               "P-PO4"),
-           stdev_spatial = c(0.4305,
-                             0.34035,
-                             0.57910,
-                             0.6568,
-                             0.49131)
+    tibble(
+        scheme = c(rep("GW: 05.1_verdro_gw: terr", 2), rep("GW: 03.3_eutr_gw", 3)),
+        target_variable = c("HG3", "LG3", "N-NH4", "N-NO3", "P-PO4"),
+        stdev_spatial = c(0.4305, 0.34035, 0.57910, 0.6568, 0.49131)
     ) %>%
     mutate(var_spatial = stdev_spatial^2)
 
-# criteria, not plotted at the moment:
+# criteria:
 scenarios <-
     crossing(
         target_variable = c("HG3", "LG3"),
@@ -38,39 +34,126 @@ scenarios <-
                 scenario = c("good", "minimal")
             )
         )
+    ) %>%
+    crossing(
+        nesting(
+            alpha = c(0.05, 0.1),
+            pi = c(0.9, 0.8),
+        )
     )
 
 # graph of relative errormargin & relative detectable difference:
-p <-
+errors <-
     modelvar %>%
-    crossing(n = 150:500) %>%
+    inner_join(
+        scenarios %>%
+            select(-rel_error, -scenario) %>%
+            distinct(),
+        join_by(target_variable),
+        relationship = "one-to-many",
+        unmatched = "error"
+    ) %>%
+    # the target variable with spatially highest variability drives sample size
+    filter(
+        stdev_spatial == max(stdev_spatial),
+        .by = scheme
+    ) %>%
+    nest(.by = alpha) %>%
+    mutate(
+        data = map2(alpha, data, function(al, df) {
+            if (al == 0.05) {
+                crossing(df, n = 300:1200)
+            } else {
+                crossing(df, n = 150:750)
+            }
+        })
+    ) %>%
+    unnest(data) %>%
     mutate(
         stdev_mu = stdev_spatial / sqrt(n),
-        log_error = qnorm(1 - 0.1 / 2) * stdev_mu,
+        log_error = qnorm(1 - alpha / 2) * stdev_mu,
         rel_error = 10^log_error - 1,
-        # detectable difference for one-sided testing (alpha = 0.1, pi = 0.8):
-        log_detectable_diff = qnorm(1 - 0.1) * stdev_mu + qnorm(0.8) * stdev_mu,
+        # detectable difference for one-sided testing:
+        log_detectable_diff = qnorm(1 - alpha) * stdev_mu + qnorm(pi) * stdev_mu,
         rel_detectable_diff = 10^log_detectable_diff - 1
     ) %>%
-    select(target_variable, n, starts_with("rel_")) %>%
+    select(scheme, n, alpha, pi, starts_with("rel_")) %>%
     pivot_longer(
       cols = starts_with("rel_"),
       names_to = "measure",
       values_to = "value"
     ) %>%
-    mutate(measure = fct_rev(measure)) %>%
-    ggplot(aes(
-      x = n,
-      y = value,
-      colour = target_variable,
-      group = target_variable
-    )) +
-    geom_line() +
+    mutate(
+        measure = replace_values(
+            measure,
+            "rel_detectable_diff" ~ str_glue("rel_detectable_diff (power: {pi})")
+        ) %>%
+            fct_relevel("rel_error")
+    ) %>%
+    select(-pi)
+
+scenarios_relerror <-
+    modelvar %>%
+    inner_join(
+        scenarios %>%
+            select(-alpha, -pi) %>%
+            distinct(),
+        join_by(target_variable),
+        relationship = "one-to-many",
+        unmatched = "error"
+    ) %>%
+    # the target variable with spatially highest variability drives sample size
+    filter(
+        stdev_spatial == max(stdev_spatial),
+        .by = scheme
+    ) %>%
+    select(scheme, rel_error, scenario)
+
+p <-
+    ggplot() +
+    geom_line(
+        data = errors %>%
+            rename(`Type I error` = alpha),
+        aes(
+            x = n,
+            y = value,
+            linetype = measure,
+            group = measure
+        )
+    ) +
     scale_y_continuous(
       breaks = seq(0, 1, 0.025),
       labels = scales::label_percent()
     ) +
-    facet_wrap(~measure)
+    facet_grid(
+      scheme ~ `Type I error`,
+      scales = "free",
+      labeller = labeller(`Type I error` = label_both)
+    ) +
+    geom_hline(
+        data = scenarios_relerror,
+        colour = "grey60",
+        aes(yintercept = rel_error, )
+    ) +
+    geom_text(
+        data = scenarios_relerror %>%
+            crossing(`Type I error` = c(0.05, 0.1)) %>%
+            mutate(x = ifelse(`Type I error` == 0.05, 300, 150)),
+        hjust = 0,
+        vjust = -0.5,
+        colour = "grey60",
+        aes(y = rel_error, label = scenario, x = x)
+    ) +
+    scale_x_continuous(breaks = seq(0, 2000, 100)) +
+    labs(
+      x = "Number of locations per typegroup",
+      y = "Value (minimal/good refers to relative error)",
+      linetype = "Quality measure"
+    ) +
+    theme(
+      legend.position = "top",
+      legend.title.position = "top"
+    )
 
 p
 
