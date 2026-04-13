3b90acc3c71b84237e5bf113451eb2ec0914f767
florisvdh <floris.vanderhaeghe@inbo.be>
2026-04-13 17:44:02 +0200

    REP: add helper script to compare two REP RData files

---
+++ 100_design_common/010_revisitplan/helper_scripts/compare_objs_from_different_RData_files_2.R
@@ -0,0 +1,40 @@
+# Comparing the contents of two supposedly identical RData files
+
+# First run setup chunk
+#
+# Then run the code to create rdata_set (see R/save_rdata.R)
+
+rdata_path_new <- file.path(
+  datapath,
+  "binary/results",
+  "objects_panflpan5.RData"
+)
+
+rdata_path_old <- file.path(
+  datapath,
+  "binary/results",
+  "objects_panflpan5_rep_0.15.0.RData"
+)
+
+load(rdata_path_new)
+oldenv <- new.env()
+load(rdata_path_old, envir = oldenv)
+ls(envir = oldenv)
+
+compare_old_new <- function(objname) {
+  isTRUE(
+    all.equal(
+      get(objname, envir = oldenv),
+      get(objname),
+      check.attributes = FALSE
+    )
+  )
+}
+
+for (i in rdata_set[1:15]) {
+  if (!compare_old_new(i)) {
+    msg <- glue::glue("Difference detected for {i}")
+    warning(msg)
+  }
+}
+
