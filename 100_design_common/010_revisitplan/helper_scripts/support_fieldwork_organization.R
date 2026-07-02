fedb63566ae12826036ac76d27f2bf17ec09ba97
florisvdh <floris.vanderhaeghe@inbo.be>
2026-07-02 09:47:35 +0200

    REP, fworg-script: create obj with watsurf geoms for lentic strata in sps

--- 100_design_common/010_revisitplan/helper_scripts/support_fieldwork_organization.R
+++ 100_design_common/010_revisitplan/helper_scripts/support_fieldwork_organization.R
@@ -132,32 +132,74 @@ scheme_moco_ps_stratum_targetpanel_spsamples %>%
 ## Sampling unit geometries --------------------------------------
 
 # obtaining geometries of the sampling units themselves:
-# - for aquatic types, see code from
-#   https://github.com/inbo/n2khab-mne-monitoring/pull/2, but then do use the
-#   POC RData file used here
+# - for lentic types, the result is in object 'watersurface_spsamples_sf' below
+# - for lotic types, see code in
+#   inbo/n2khab-mne-monitoring/010_aq_piezometer_positioning, but then do use
+#   the REP RData file used here
 # - for type 7220 (springs) as a whole, see code provided below
 # - for terrestrial types, these are cells; see code provided below
 
 
 
-# helper object to add watersurface polygon_ids in a geopackage (this may be
-# dropped when properly adding the geometries here)
-# ////////////////////////////////////////////////////////////////////////////
+# geometries of the spatial sampling units of lentic types (polygons)
+# /////////////////////////////////////////////////////////////////////////
 
-set.seed(20260618)
-watersurface_polygon_ids <-
-  stratum_units_non_cell_n2khab %>%
-  filter(sample_support_code == "watersurface") %>%
+# link between spatial sampling units of lentic types and watersurface polygons
+stratum_grts_polygon_spsamples_lentic <-
+  scheme_moco_ps_stratum_sppost_spsamples %>%
+  filter(str_detect(stratum, "^2190_a|^31")) %>%
+  unnest(sp_poststr_samples) %>%
+  select(-sample_status) %>%
+  # provision for potential local replacements by including grts_address_final
+  # (currently not different for these strata)
+  add_assessment_data() %>%
+  distinct(stratum, grts_address_final) %>%
   inner_join(
-    units_non_cell_n2khab_grts,
-    join_by(sample_support_code, unit_id),
+    units_non_cell_n2khab_grts %>%
+      filter(sample_support_code == "watersurface") %>%
+      select(-sample_support_code),
+    join_by(grts_address_final == grts_address),
     relationship = "many-to-one",
     unmatched = c("error", "drop")
   ) %>%
-  select(stratum, grts_address, polygon_id = unit_id) %>%
-  # following step needs extra attention to store choices (MNE+MHQ), preferably
-  # a ranking
-  slice_sample(n = 1, by = c(stratum, grts_address))
+  rename(polygon_id = unit_id)
+
+# extract all polygons from watersurfaces_hab that belong to the spatial samples
+# for lentic types:
+wsh_polygons_spsamples <-
+  read_watersurfaces_hab(version = versions_required["watersurfaces_hab"]) %>%
+  pluck("watersurfaces_polygons") %>%
+  select(polygon_id) %>%
+  semi_join(stratum_grts_polygon_spsamples_lentic, join_by(polygon_id))
+
+# extract all polygons from the watersurfaces data source that belong to the
+# spatial samples for lentic types but that were not covered by
+# watersurfaces_hab:
+ws_extra_polygons_spsamples <-
+  read_watersurfaces(
+    version = versions_required["watersurfaces"],
+    fix_geom = TRUE
+  ) %>%
+  select(polygon_id) %>%
+  semi_join(stratum_grts_polygon_spsamples_lentic, join_by(polygon_id)) %>%
+  # keeping only the unique extra polygons relative to watersurfaces_hab
+  anti_join(
+    st_drop_geometry(wsh_polygons_spsamples),
+    join_by(polygon_id)
+  )
+
+# add the geometry to the spatial sampling units:
+stratum_grts_spsamples_lentic_sf <-
+  rbind(wsh_polygons_spsamples, ws_extra_polygons_spsamples) %>%
+  inner_join(
+    stratum_grts_polygon_spsamples_lentic,
+    join_by(polygon_id),
+    relationship = "one-to-many",
+    unmatched = "error"
+  ) %>%
+  relocate(stratum, grts_address_final) %>%
+  arrange(stratum, grts_address_final)
+
 
 
 # geometries of 7220 units are represented by points, labelled with their GRTS
@@ -1855,6 +1897,8 @@ objects <- tibble(
     "stratum_schemepstargetpanel_spsamples",
     "schemepstargetpanel_spsamples_terr",
     "vbi_overlaps",
+    "stratum_grts_polygon_spsamples_lentic",
+    "stratum_grts_spsamples_lentic_sf",
     "units_7220",
     "units_cell_cellcenter",
     "units_cell_rast",
