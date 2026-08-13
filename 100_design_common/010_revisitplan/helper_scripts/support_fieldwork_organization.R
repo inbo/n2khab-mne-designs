efd66d51ac68595f6c414a32e992b633ff8ee19b
florisvdh <floris.vanderhaeghe@inbo.be>
2026-08-13 10:28:14 +0200

    REP, fworg-script: add 'if (interactive())' to some 'printing' chunks

--- 100_design_common/010_revisitplan/helper_scripts/support_fieldwork_organization.R
+++ 100_design_common/010_revisitplan/helper_scripts/support_fieldwork_organization.R
@@ -119,9 +119,11 @@ stratum_schemepstargetpanel_spsamples <-
 # place using grts_address as the anchor, provided that the type still occurs in
 # the polygon. If not, the absence must be noted and sampling frame + sample are
 # to be updated.
+if (interactive()) {
 scheme_moco_ps_stratum_targetpanel_spsamples %>%
   filter(grts_address != grts_address_final) %>%
   glimpse()
+}
 
 
 
@@ -148,7 +150,7 @@ scheme_moco_ps_stratum_targetpanel_spsamples %>%
 # /////////////////////////////////////////////////////////////////////////
 
 # The geometries of lentic spatial sampling units are in following object.
-glimpse(stratum_grts_spsamples_lentic_sf)
+if (interactive()) glimpse(stratum_grts_spsamples_lentic_sf)
 
 # The spatial sampling unit is always identified by stratum x grts_address by
 # definition. Note that, as usual, grts_address_final is the GRTS address linked
@@ -454,7 +456,7 @@ units_cell_polygon %>%
 # locations (grts_address_final) that apply to the (current) lentic sampling
 # units. Notably, these points serve as a candidate for re-use.
 
-glimpse(legacy_watersamplepoints_spslocs_lentic)
+if (interactive()) glimpse(legacy_watersamplepoints_spslocs_lentic)
 
 # As usual, the stable column to identify the polygons is grts_address_final and
 # the corresponding (less stable) polygon_id is for information (see higher:
@@ -1111,7 +1113,7 @@ actseqs_actgroups_acts <-
   )
 
 
-fag_stratum_grts_calendar
+if (interactive()) fag_stratum_grts_calendar
 
 # fag_stratum_grts_calendar defines the needed visits of the spatial sampling
 # units and is organized at the FAG level. The rank is an indication of the
@@ -1145,7 +1147,7 @@ fag_fa_stratum_grts_calendar <-
 # during which the auxiliary FAG (in the scheduled time interval) is still
 # relevant to subsequent FAGs in that scheme.
 
-cal_old_continuation
+if (interactive()) cal_old_continuation
 
 # cal_old_continuation is a subset of fag_stratum_grts_calendar (without
 # assessment columns) that represents GWSHALL* and READDIVER FAG occasions in
